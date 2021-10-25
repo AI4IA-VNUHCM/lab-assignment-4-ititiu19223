6a8de9297654480c31bd42a566b1d5296b1382fe
ititiu19223 <ititiu19223@student.hcmiu.edu.vn>
2021-10-25 13:11:06 +0700

    Ex 1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -15,15 +15,31 @@ ______________________________________
 #include <stdlib.h>
 #include <math.h>
 
-void Ex1(int n){
-	//Your codes here
-    
+void Ex1(int n)
+{
+	int arr[n][n];
+
+	// Iterate through every line and
+	for (int line = 0; line < n + 1; line++)
+	{
+		for (int i = 0; i <= line; i++)
+		{
+			if (line == i || i == 0)
+				arr[line][i] = 1;
+			else
+				arr[line][i] = arr[line - 1][i - 1] +
+							   arr[line - 1][i];
+			printf("%d", arr[line][i]);
+		}
+		printf("\n");
+	}
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int testcase = atoi(argv[1]);
-	
+
 	Ex1(testcase);
 	return 0;
 }
