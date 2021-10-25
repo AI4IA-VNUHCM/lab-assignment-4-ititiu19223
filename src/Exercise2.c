075b11ceba22015b130224ba61ec81a219c8b1c5
ititiu19223 <ititiu19223@student.hcmiu.edu.vn>
2021-10-25 14:59:52 +0700

    Ex 1, 2

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -29,8 +29,10 @@ void Array2Dconverter(int arr[], int a[SIZE][SIZE], int m, int n)
 	int row, column;
 	int counter = 0;
 	//Convert 1D array to 2D array
-	for (row = 0; row <= (m - 1); row ++){
-		for (column = 0; column <= (n - 1); column ++){
+	for (row = 0; row <= (m - 1); row++)
+	{
+		for (column = 0; column <= (n - 1); column++)
+		{
 			a[row][column] = arr[counter];
 			counter++;
 		}
@@ -41,33 +43,69 @@ void printArray(int a[SIZE][SIZE], int m, int n)
 {
 	int row, column;
 
-	for (row = 0; row <= (m - 1); row ++){
-		for (column = 0; column <= (n - 1); column ++){
+	for (row = 0; row <= (m - 1); row++)
+	{
+		for (column = 0; column <= (n - 1); column++)
+		{
 			printf("%d ", a[row][column]);
 		}
 		printf("\n");
 	}
 }
 
-void Ex2(int arr[], int m, int n){
+void Ex2(int arr[], int m, int n)
+{
 	int a[SIZE][SIZE];
-	Array2Dconverter(arr,a,m,n);
+	Array2Dconverter(arr, a, m, n);
 	//Your codes here
-
+	int i, x, j, temp;
+	for (i = 0; i < n; i++)
+	{
+		for (x = 0; x < m; x++)
+		{
+			if (i % 2 != 0)
+			{
+				for (j = x + 1; j < m; j++)
+				{
+					if (a[x][i] > a[j][i])
+					{
+						temp = a[x][i];
+						a[x][i] = a[j][i];
+						a[j][i] = temp;
+					}
+				}
+			}
+			else
+			{
+				for (j = x + 1; j < m; j++)
+				{
+					if (a[x][i] < a[j][i])
+					{
+						temp = a[x][i];
+						a[x][i] = a[j][i];
+						a[j][i] = temp;
+					}
+				}
+			}
+		}
+		printf("\n");
+	}
 	printArray(a, m, n);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int row = atoi(argv[1]);
 	int col = atoi(argv[2]);
-	argc-=3;
-	int testcase[argc],i;
-	for(i=0; i<argc;i++){
-		testcase[i] = atoi(argv[i+3]);
+	argc -= 3;
+	int testcase[argc], i;
+	for (i = 0; i < argc; i++)
+	{
+		testcase[i] = atoi(argv[i + 3]);
 	}
 
 	Ex2(testcase, row, col);
-	
+
 	return 0;
 }
